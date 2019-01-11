c6fe3306f50e776dd681ff62f0d0f8e179993b3f
irfan339 <irfansultan339@gmail.com>
2019-01-11 12:47:18 +0530

    hi

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-//import java.io.*;
-
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-       System.out.println("Hello World from Master Branch");
-    }//End of Main
-}//End of class
