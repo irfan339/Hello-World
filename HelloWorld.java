80b954e687087723029f40dc03b91a1407daa937
irfan339 <irfansultan339@gmail.com>
2019-01-11 12:46:06 +0530

    first Commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//import java.io.*;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+       System.out.println("Hello World from Master Branch");
+    }//End of Main
+}//End of class
